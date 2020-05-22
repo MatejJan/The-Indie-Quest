81754830240585707ff4469c9032ce6a7d5034bd
Matej Jan <matej@retronator.com>
2020-05-22 21:11:48 +0200

    Day 1 of week 3.

---
+++ Week 3/Day 1/JoinWithAnd/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JoinWithAnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var heroes = new List<string> { "Jazlyn", "Theron", "Dayana", "Rolando" };
+
+            while (heroes.Count > 0)
+            {
+                Console.WriteLine($"The heroes in the party are: {JoinWithAnd(heroes, false)}.");
+                heroes.RemoveAt(0);
+            }
+        }
+
+        static string JoinWithAnd(List<string> items, bool useSerialComma = true)
+        {
+            int count = items.Count;
+
+            if (count == 0) return "";
+            if (count == 1) return items[0];
+            if (count == 2) return $"{items[0]} and {items[1]}";
+
+            var itemsCopy = new List<string>(items);
+
+            if (useSerialComma)
+            {
+                itemsCopy[count - 1] = $"and {items[count - 1]}";
+            }
+            else
+            {
+                itemsCopy[count - 2] = $"{items[count - 2]} and {items[count - 1]}";
+                itemsCopy.RemoveAt(count - 1);
+            }
+
+            return String.Join(", ", itemsCopy);
+        }
+    }
+}
