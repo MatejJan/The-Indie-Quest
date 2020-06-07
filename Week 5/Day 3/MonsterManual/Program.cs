9c0b5279b6e52f4404c682b772190102fd2759a6
Matej Jan <matej@retronator.com>
2020-06-07 22:47:28 +0200

    Week 5 assignments.

---
+++ Week 5/Day 3/MonsterManual/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace MonsterManual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("MonsterManual.txt");
+
+            var axis1Values = new[] { "lawful", "neutral", "chaotic" };
+            var axis2Values = new[] { "good", "neutral", "evil" };
+
+            var namesByAlignment = new List<string>[3, 3];
+            var namesOfUnaligned = new List<string>();
+            var namesOfAnyAlignment = new List<string>();
+            var namesOfSpecialCases = new List<string>();
+            var specialCases = new List<string>();
+
+            for (int axis1 = 0; axis1 < 3; axis1++)
+                for (int axis2 = 0; axis2 < 3; axis2++)
+                    namesByAlignment[axis1, axis2] = new List<string>();
+
+            int monsterBlockLineIndex = 0;
+            string currentName = "";
+
+            foreach (string line in lines)
+            {
+                monsterBlockLineIndex++;
+
+                // Parse the name.
+                if (monsterBlockLineIndex == 1)
+                {
+                    currentName = line;
+                }
+
+                // Parse the alignment line.
+                if (monsterBlockLineIndex == 2)
+                {
+                    Match match = Regex.Match(line, @"(lawful|neutral|chaotic) (good|neutral|evil)");
+
+                    if (match.Success)
+                    {
+                        int axis1 = Array.IndexOf(axis1Values, match.Groups[1].Value);
+                        int axis2 = Array.IndexOf(axis2Values, match.Groups[2].Value);
+
+                        namesByAlignment[axis1, axis2].Add(currentName);
+                        continue;
+                    }
+
+                    if (line.Contains("neutral"))
+                    {
+                        namesByAlignment[1, 1].Add(currentName);
+                        continue;
+                    }
+
+                    if (line.Contains("unaligned"))
+                    {
+                        namesOfUnaligned.Add(currentName);
+                        continue;
+                    }
+
+                    if (line.Contains("any alignment"))
+                    {
+                        namesOfAnyAlignment.Add(currentName);
+                        continue;
+                    }
+
+                    namesOfSpecialCases.Add(currentName);
+
+                    string[] parts = line.Split(", ");
+                    specialCases.Add(parts[1]);
+
+                    continue;
+                }
+
+                if (line.Length == 0)
+                {
+                    monsterBlockLineIndex = 0;
+                }
+            }
+
+            for (int axis2 = 0; axis2 < 3; axis2++)
+            {
+                for (int axis1 = 0; axis1 < 3; axis1++)
+                {
+                    if (axis1 == 1 && axis2 == 1)
+                    {
+                        Console.WriteLine($"Monsters with alignment true neutral are:");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Monsters with alignment {axis1Values[axis1]} {axis2Values[axis2]} are:");
+                    }
+
+                    foreach (string name in namesByAlignment[axis1, axis2]) Console.WriteLine(name);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine($"Unaligned monsters are:");
+            foreach (string name in namesOfUnaligned) Console.WriteLine(name);
+            Console.WriteLine();
+
+            Console.WriteLine($"Monsters which can be of any alignment are:");
+            foreach (string name in namesOfAnyAlignment) Console.WriteLine(name);
+            Console.WriteLine();
+
+            Console.WriteLine($"Monsters with special cases are:");
+            for (int i = 0; i < namesOfSpecialCases.Count; i++)
+            {
+                Console.WriteLine($"{namesOfSpecialCases[i]} ({specialCases[i]})");
+            }
+        }
+    }
+}
---
+++ Week 5/Day 4/SteamTracker/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SteamTracker
+{
+    class Program
+    {
+        static void Main()
+        {
+            var httpClient = new HttpClient();
+
+            string[] gameUrls = new[]
+            {
+                @"https://store.steampowered.com/app/266390/Farm_for_your_Life/",
+                @"https://store.steampowered.com/app/427520/Factorio/",
+                @"https://store.steampowered.com/app/546430/Pathway",
+                @"https://store.steampowered.com/app/305660/Infect_and_Destroy/",
+                @"https://store.steampowered.com/app/275850/No_Mans_Sky/"
+            };
+
+            foreach (string gameUrl in gameUrls)
+            {
+                string websiteCode = httpClient.GetStringAsync(gameUrl).Result;
+
+                Match match = Regex.Match(websiteCode, @"apphub_AppName.*>(.*)<");
+                string gameName = match.Groups[1].Value;
+                Console.WriteLine(gameName.ToUpperInvariant());
+
+                match = Regex.Match(websiteCode, @"Recent Reviews.*\n.*\n.*game_review_summary.*>(.*)<");
+                if (match.Success)
+                {
+                    string recentReviews = match.Groups[1].Value;
+                    Console.WriteLine($"Recent reviews: {recentReviews}");
+                }
+
+                match = Regex.Match(websiteCode, @"All Reviews.*\n.*\n.*game_review_summary.*>(.*)<");
+                string allReviews = match.Groups[1].Value;
+                Console.WriteLine($"All reviews: {allReviews}");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
