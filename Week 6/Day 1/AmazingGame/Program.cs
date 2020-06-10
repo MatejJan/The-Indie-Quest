02c9dfeac37de55f87d6427782d6602446490ef2
Matej Jan <matej@retronator.com>
2020-06-10 18:41:24 +0200

    Week 6, days 1 and 2.

---
+++ Week 6/Day 1/AmazingGame/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+
+namespace AmazingGame
+{
+    class Program
+    {
+        static int width;
+        static int height;
+        static char[,] map;
+
+        static int playerX;
+        static int playerY;
+
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("MazeLevel.txt");
+
+            string levelName = lines[0];
+
+            string[] dimensionsText = lines[1].Split('x');
+
+            width = int.Parse(dimensionsText[0]);
+            height = int.Parse(dimensionsText[1]);
+
+            map = new char[width, height];
+
+            var random = new Random();
+
+            for (int y = 0; y < height; y++)
+            {
+                string line = lines[y + 2];
+
+                for (int x = 0; x < width; x++)
+                {
+                    char symbol = line[x];
+
+                    if (symbol == 'S')
+                    {
+                        playerX = x;
+                        playerY = y;
+                        map[x, y] = ' ';
+                        continue;
+                    }
+
+                    if (y < 3 && random.Next((y + 1) * 2) == 0)
+                    {
+                        symbol = '♠';
+                    }
+
+                    map[x, y] = symbol;
+                }
+            }
+
+            Console.WriteLine($"Get ready for: {levelName}");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to start …");
+
+            Console.ReadKey();
+
+            while (true)
+            {
+                DrawMap();
+
+                if (map[playerX, playerY] == 'M')
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("You have reached the minotaur. You are the winner!");
+                    return;
+                }
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                int newPlayerX = playerX;
+                int newPlayerY = playerY;
+
+                if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    newPlayerY++;
+                }
+                else if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    newPlayerY--;
+                }
+                else if (keyInfo.Key == ConsoleKey.RightArrow)
+                {
+                    newPlayerX++;
+                }
+                else if (keyInfo.Key == ConsoleKey.LeftArrow)
+                {
+                    newPlayerX--;
+                }
+                else if (keyInfo.Key == ConsoleKey.Escape)
+                {
+                    return;
+                }
+
+                // Only allow to move into the space if it's empty or it's the minotaur.
+                char newMapLocation = map[newPlayerX, newPlayerY];
+                if (newMapLocation == ' ' || newMapLocation == 'M')
+                {
+                    playerX = newPlayerX;
+                    playerY = newPlayerY;
+                }
+            }
+        }
+
+        static void DrawMap()
+        {
+            Console.Clear();
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (x == playerX && y == playerY)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write('☺');
+                        continue;
+                    }
+
+                    char symbol = map[x, y];
+
+                    if (symbol == 'M')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    else if (symbol == '♠')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Week 6/Day 2/DiceSimulator/Program.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace DiceSimulator
+{
+    class Program
+    {
+        const int maximumSides = 20;
+
+        static string[] diceArt = new string[maximumSides + 1];
+        static ConsoleColor[] diceColors = new ConsoleColor[maximumSides + 1];
+
+        static void Main(string[] args)
+        {
+            // Load dice ASCII art.
+            string[] lines = File.ReadAllLines("Dice.txt");
+
+            string currentDiceArt = "";
+            int currentDiceSides = 0;
+
+            foreach (string line in lines)
+            {
+                if (line.Length == 0)
+                {
+                    // Store dice art.
+                    diceArt[currentDiceSides] = currentDiceArt;
+                    continue;
+                }
+
+                Match match = Regex.Match(line, @"(\d+) (.+)");
+
+                if (match.Success)
+                {
+                    // This is a description of a new dice.
+                    currentDiceSides = int.Parse(match.Groups[1].Value);
+
+                    int colorNumber = int.Parse(match.Groups[2].Value);
+                    ConsoleColor color = (ConsoleColor)colorNumber;
+
+                    diceColors[currentDiceSides] = color;
+                    currentDiceArt = "";
+                }
+                else
+                {
+                    // This is a new line of dice art.
+                    currentDiceArt += $"{line}\n";
+                }
+            }
+
+            // Run the simulator for the first time.
+            string lastDiceText = RunSimulator();
+
+            do
+            {
+                // See if the user wants to do another roll.
+                Console.WriteLine();
+                Console.WriteLine("Do you want to (r)epeat, enter a (n)ew roll, or (q)uit?");
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                if (keyInfo.Key == ConsoleKey.R)
+                {
+                    lastDiceText = RunSimulator(lastDiceText);
+                }
+                else if (keyInfo.Key == ConsoleKey.N)
+                {
+                    lastDiceText = RunSimulator();
+                }
+                else
+                {
+                    return;
+                }
+
+            } while (true);
+        }
+
+        static string RunSimulator(string diceText = null)
+        {
+            Console.Clear();
+            Console.WriteLine("DICE SIMULATOR");
+            Console.WriteLine();
+
+            int numberOfRolls, diceSides, fixedBonus;
+
+            Console.WriteLine("Enter desired dice roll in standard dice notation:");
+
+            do
+            {
+                if (diceText == null)
+                {
+                    diceText = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine(diceText);
+                }
+
+                Console.WriteLine();
+
+                Match match = Regex.Match(diceText, @"(\d+)?d(\d+)([+-]\d+)?");
+
+                if (match.Success)
+                {
+                    if (match.Groups[1].Success)
+                    {
+                        numberOfRolls = int.Parse(match.Groups[1].Value);
+                    }
+                    else
+                    {
+                        numberOfRolls = 1;
+                    }
+
+                    diceSides = int.Parse(match.Groups[2].Value);
+
+                    if (match.Groups[3].Success)
+                    {
+                        fixedBonus = int.Parse(match.Groups[3].Value);
+                    }
+                    else
+                    {
+                        fixedBonus = 0;
+                    }
+
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You did not use standard dice notation. Try again:");
+                    diceText = null;
+                }
+
+            } while (true);
+
+            Console.WriteLine("Simulating …");
+            Console.WriteLine();
+            Thread.Sleep(500);
+
+            var random = new Random();
+            int sum = 0;
+
+            for (int rollIndex = 1; rollIndex <= numberOfRolls; rollIndex++)
+            {
+                int rolledNumber = random.Next(diceSides) + 1;
+
+                if (diceArt[diceSides] != null)
+                {
+                    string currentDiceArt = diceArt[diceSides];
+
+                    // Replace number placeholders.
+                    int writtenNumber = rolledNumber;
+                    if (diceSides == 10 && rolledNumber == 10) writtenNumber = 0;
+
+                    currentDiceArt = currentDiceArt.Replace("??", $"{writtenNumber,2}").Replace("?", $"{writtenNumber}");
+
+                    // Draw the dice in correct color.
+                    Console.ForegroundColor = diceColors[diceSides];
+                    Console.WriteLine(currentDiceArt);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.WriteLine($"{OrdinalNumber(rollIndex)} roll is: {rolledNumber}");
+                }
+
+                Thread.Sleep(500);
+
+                sum += rolledNumber;
+            }
+
+            Console.WriteLine();
+
+            if (fixedBonus == 0)
+            {
+                Console.WriteLine($"You rolled {sum}.");
+            }
+            else
+            {
+                int score = sum + fixedBonus;
+
+                if (fixedBonus > 0)
+                {
+                    Console.WriteLine($"You rolled {sum}. Together with the bonus {fixedBonus}, the result is {score}.");
+                }
+                else
+                {
+                    Console.WriteLine($"You rolled {sum}. Together with the penalty {fixedBonus}, the result is {score}.");
+                }
+            }
+
+            return diceText;
+        }
+
+        static string OrdinalNumber(int number)
+        {
+            int lastDigit = number % 10;
+
+            if (number > 10)
+            {
+                int secondToLastDigit = (number / 10) % 10;
+                if (secondToLastDigit == 1) return $"{number}th";
+            }
+
+            if (lastDigit == 1) return $"{number}st";
+            if (lastDigit == 2) return $"{number}nd";
+            if (lastDigit == 3) return $"{number}rd";
+
+            return $"{number}th";
+        }
+    }
+}
