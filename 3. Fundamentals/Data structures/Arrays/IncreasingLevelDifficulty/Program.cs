86270cd6eaaeb3511922abaa52b64ad8c6575240
Matej Jan <matej@retronator.com>
2021-09-27 15:58:49 +0200

    Arrays mission 2.

---
+++ 3. Fundamentals/Data structures/Arrays/IncreasingLevelDifficulty/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IncreasingLevelDifficulty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var monstersPerLevel = new int[100];
+
+            for (int i = 0; i < monstersPerLevel.Length; i++)
+            {
+                monstersPerLevel[i] = random.Next(1, 51);
+            }
+
+            Array.Sort(monstersPerLevel);
+
+            Console.WriteLine($"Number of monsters in levels: {String.Join(", ", monstersPerLevel)}");
+        }
+    }
+}
